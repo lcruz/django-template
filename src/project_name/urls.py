9d6d703c4e34bafb2a8d40f2beadb7fece6cfdd0
Luis Cruz <lcruzc@gmail.com>
2020-04-23 18:30:19 -0400

    fix: renamed project folder and added tags

--- src/project_name/urls.py
+++ src/project_name/urls.py
@@ -1,4 +1,4 @@
-"""djangotemplate URL Configuration
+"""{{ project_name }} URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
